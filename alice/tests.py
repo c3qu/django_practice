e8ddaf8b3965ed78e4434c36aa4e2e6952f2f3c0
tantalum <c3qu@qq.com>
2023-08-29 23:20:49 +0800

    personandjon=None

--- alice/tests.py
+++ alice/tests.py
@@ -1,13 +1,19 @@
 # Create your tests here.
 from django.db.models import Sum, When, Case
-from django.test import TestCase
-
-from alice.models import PersonAndJob
+from alice.models import PersonAndJob, Person
 
 
+# python .\manage.py shell -c "from alice import tests;tests.test_group_by()"
 def test_group_by():
     """Animals that can speak are correctly identified"""
     person_and_job = PersonAndJob.objects.values("person").annotate(
-        cnt=Sum(Case(When(salary=0, then=0), default=1))).filter(cnt=0)
-
+        cnt=Sum(Case(
+            When(salary=0, then=0),
+            default=1
+        ))).filter(cnt=0)
     print(person_and_job)
+
+
+def test_no():
+    cc=Person.objects.filter(personandjob=None)
+    print(cc)
